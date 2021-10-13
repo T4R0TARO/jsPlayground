ef5083f26cc25207d0547b47fbd101fe32f1a536
josh manansala <joshmanansalagsd@gmail.com>
2021-10-12 17:58:13 -0700

    callbacks pract

--- script.js
+++ script.js
@@ -1,170 +1,66 @@
+console.log('wah');
 
-const toppings = ['Mushrooms ', 'Tomatoes', 'Eggs', 'Chili', 'Lettuce', 'Avocado', 'Chiles', 'Bacon', 'Pickles', 'Onions', 'Cheese'];
-
-const students = [
-  {
-    id: '11ce',
-    first_name: 'Dall',
-    last_name: 'Puckring',
-  },
-  {
-    id: '2958',
-    first_name: 'Margarete',
-    last_name: 'Brandi',
-  },
-  {
-    id: '565a',
-    first_name: 'Bendicty',
-    last_name: 'Woodage',
-  },
-  {
-    id: '3a16',
-    first_name: 'Micki',
-    last_name: 'Mattes',
-  },
-  {
-    id: 'f396',
-    first_name: 'Flory',
-    last_name: 'Gladeche',
-  },
-  {
-    id: 'de5f',
-    first_name: 'Jamill',
-    last_name: 'Emilien',
-  },
-  {
-    id: '54cb',
-    first_name: 'Brett',
-    last_name: 'Aizikowitz',
-  },
-  {
-    id: '9135',
-    first_name: 'Lorry',
-    last_name: 'Smallman',
-  },
-  {
-    id: '978f',
-    first_name: 'Gilly',
-    last_name: 'Flott',
-  },
-];
-
-const people = [
-  {
-    birthday: 'April 22, 1993',
-    names: {
-      first: 'Keith',
-      last: 'Buckley'
-    }
-  },
-  {
-    birthday: 'January 3, 1975',
-    names: {
-      first: 'Larry',
-      last: 'Heep'
-    }
-  },
-  {
-    birthday: 'February 12, 1944',
-    names: {
-      first: 'Linda',
-      last: 'Bermeer'
-    }
-  }
-];
+//Callback Function
 
 
-const buns = ['egg', 'wonder', 'brioche'];
+// The function that will be called later in the future (callback)
+function sayHello() {
+    console.log('Hello');
+}
+// The function containing the callback function
+// function can be used as a param. as well
+// In this case all functions that fire through this function is a callback 
+function executeCallback( callback ){
+    callback();
+}
+// Function fires callback function
+executeCallback(sayHello);
 
-const meats = {
-  beyond: 10,
-  beef: 5,
-  pork: 7
-};
+ 
+// More Callbacks
+const div1 = document.getElementById('first');
+const div2 = document.getElementById('second');
+const div3 = document.getElementById('third');
 
-const prices = {
-  hotDog: 453,
-  burger: 765,
-  sausage: 634,
-  corn: 234,
-};
-
-const orderTotals = [342, 1002, 523, 34, 634, 854, 1644, 2222];
-
-const feedback = [
-  { comment: 'Love the burgs', rating: 4 },
-  { comment: 'Horrible Service', rating: 2 },
-  { comment: 'Smoothies are great, liked the burger too', rating: 5 },
-  { comment: 'Ambiance needs work', rating: 3 },
-  { comment: 'I DONT LIKE BURGERS', rating: 1 },
-];
-
-const faces = ['😃', '🤠', '🤡', '🤑', '😵', '🌞', '🐶', '😺'];
-
-const inventory = [
-  { type: 'shirt', price: 4000 },
-  { type: 'pants', price: 4532 },
-  { type: 'socks', price: 234 },
-  { type: 'shirt', price: 2343 },
-  { type: 'pants', price: 2343 },
-  { type: 'socks', price: 542 },
-  { type: 'pants', price: 123 },
-];
-
-
-/*
-  Looping Methods forEach()
-*/
-
-function logToppings(topping, index, originalArray){
-    const prevTopping = originalArray[index - 1];
-    const nextTopping = originalArray[index + 1];
-    
-    //log topping
-    console.log(topping);
-    
-    //log prev topping
-    prevTopping ? console.log(prevTopping) : null;
-    
-    //log next topping
-    nextTopping ? console.log(nextTopping) : null;
-
-    //if last item say "good bye"
-    index === originalArray.length - 1 
-     ? console.log('good bye')
-     : console.log('prepare next item');
-       console.log('---🍖----');
-   }
-    toppings.forEach(logToppings);
-
-/*  Arrow Function 
-  toppings.forEach(topping => {
-      console.log(topping);
-    });
-*/
-
-//  Map, Filter, Reduce
-    console.clear();
-
-function addArms(face) {
-return `🦾${face}✌`
+function makeRed(element) {
+    element.style.backgroundColor = "red";
 }
 
-const toys = faces.map(addArms);
-console.log(toys);
-
-function bosify(name) {
-return `${name} Bos`;
+function makeBlue(element) {
+    element.style.backgroundColor = "blue";
 }
 
-function capitalize(word) {
-return `${word[0].toUpperCase()}${word.slice(1)}`;
+function makeGreen(element) {
+  element.style.backgroundColor = "Green"
 }
 
-const fullNames = ['wes', 'kait', 'poppy']
-.map(capitalize)
-.map(bosify);
-console.log(fullNames);
+function addStyleToElement(element, callback) {
+    callback(element);
+}
 
-const orderTotalsWithTax = orderTotals.map(total => total * 1.13);
-console.log(orderTotalsWithTax);
+addStyleToElement(div1, makeRed);
+
+addStyleToElement(div2, makeBlue);
+
+addStyleToElement(div3 , makeGreen);
+
+// More Callbacks 
+
+const nameInput = document.getElementById('name');
+const messageTextArea = document.getElementById('message');
+
+
+const focusHandler =  event => {
+    event.target.className = 'highlight';
+  };
+
+const blurHandler =  event => {
+    event.target.className = '';
+  };
+
+ nameInput.addEventListener('focus', focusHandler);
+ nameInput.addEventListener('blur', blurHandler);
+
+
+ messageTextArea.addEventListener('focus',  focusHandler);
+ messageTextArea.addEventListener('blur', blurHandler);
