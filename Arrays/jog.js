3bb5f0af9d0b07da747a41c69d43d7dc02861f7e
josh manansala <joshmanansalagsd@gmail.com>
2021-12-01 19:42:32 -0800

    array cardio

---
+++ Arrays/jog.js
@@ -0,0 +1,6 @@
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const array2 = ['a', 'b', 'c', 'd', 'e']
+
+/*map()*/
+// creates a new array populated with the results of calling  a provided function on every element in the calling array
+
