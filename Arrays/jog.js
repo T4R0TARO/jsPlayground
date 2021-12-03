d0beccd946f55e810e7b987f753403e535689215
josh manansala <joshmanansalagsd@gmail.com>
2021-12-02 21:33:20 -0800

    array cardio

--- Arrays/jog.js
+++ Arrays/jog.js
@@ -4,3 +4,12 @@ const array2 = ['a', 'b', 'c', 'd', 'e']
 /*map()*/
 // creates a new array populated with the results of calling  a provided function on every element in the calling array
 
+arrayDoubler = array1.map( x => x + x)
+console.log(arrayDoubler)
+
+/*reduce()*/
+// reduce the array to a single value. In this case, the sum of the array.
+
+const reducer = (a,b) => a + b;
+const arraySum = array1.reduce(reducer)
+console.log(arraySum)
