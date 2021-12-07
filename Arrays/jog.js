b96a0d0992c952fe744e51e0a2d1cb496b01b4fd
josh manansala <joshmanansalagsd@gmail.com>
2021-12-06 18:13:26 -0800

    array cardio

--- Arrays/jog.js
+++ Arrays/jog.js
@@ -6,27 +6,56 @@ const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']
 // creates a new array populated with the results of calling  a provided function on every element in the calling array
 
 arrayDoubler = array1.map( x => x + x)
-console.log(arrayDoubler)
+// console.log(arrayDoubler)
 
 /*reduce()*/
 // creates a new array and reduces the array to a single value. In this case, the sum of the array.
 
 const reducer = (a,b) => a + b;
 const arraySum = array1.reduce(reducer)
-console.log(arraySum)
-console.log(array1)
+// console.log(arraySum)
+// console.log(array1)
 
 
 /*filter()*/
 // creates a new array with all elements that pass the test provided by the function
 const results = words.filter(x => x.length > 10)
-console.log(results)
+// console.log(results)
 
 /*slice()*/
 // returns shallow copy of a portion of an array into a new array selected from `start` to `end`. The original array will not be modified
 const sliceArray = words.slice(1, 3)
-console.log(sliceArray)
-console.log(words)
+// console.log(sliceArray)
+// console.log(words)
 
 /*splice() */
 // changes the contents of an array by removing or replacing existing elements
+const spliceArray = words.splice(0, 3)
+console.log(spliceArray)
+console.log(words)
+
+/*concat() */
+// used to merge two or more arrays. This method does not change the existing array;
+const concatArray = array2.concat(words)
+console.log(concatArray)
+
+/*push() */
+// adds one or more elements to the end of an array and returns the new length
+
+
+/*forEach() */
+// executes a provided funtion once for each array element
+
+spliceArray.forEach((element) => {
+    words.push(element)
+})
+
+console.log(words)
+
+/**sort() */
+// sorts the elements of an array and returns the sorted array. The default sort ascending.
+words.sort();
+console.log(words)
+
+
+
