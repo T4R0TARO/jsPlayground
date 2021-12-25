5b9e0039bf14138e542ee95d969f82d735305d88
josh manansala <joshmanansalagsd@gmail.com>
2021-12-24 19:22:09 -0800

    re-intro javascript

--- re-intro-js/script.js
+++ re-intro-js/script.js
@@ -1,1 +1,26 @@
-console.log('wah');
+l//JS DATA TYPES
+/**
+ * Numbers
+ * Strings
+ * Booleans
+ * BigInt
+ * Function
+ * Objects
+ * Arrays
+ * Variables
+ * Undefined
+ * Null
+ * Symbols***
+ */
+
+console.log( 3 / 2 );
+console.log(Math.floor(3 / 2));
+
+console.log(parseInt('123'));
+console.log(parseInt(123));
+
+console.log('hello'.length);
+console.log('hello'.charAt(1));
+console.log('hello, world'.replace('world', 'wah'));
+console.log('hello'.toUpperCase()); 
+
