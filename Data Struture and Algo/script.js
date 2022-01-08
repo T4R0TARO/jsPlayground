6a5c4e449bc8ce858b6a37f8700978c718f67f7e
josh manansala <joshmanansalagsd@gmail.com>
2022-01-07 16:12:41 -0800

    algo patterns

--- Data Struture and Algo/script.js
+++ Data Struture and Algo/script.js
@@ -190,3 +190,41 @@ function validAnagram(first, second){
         }
     }
 }
+
+//countUniqueValues
+/*
+Implement a function called countUniqueValues, which accepts a sorted away, and counts the unique values in the array. 
+There can be negative unique values in the array. There can be negative numbers in the array but, it will always 
+sorted
+*/
+
+//NAIVE
+function sumZero(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+sumZero([-4,-3,-2,-1,0,1,2,5])
+
+// SUM ZERO  REFACTOR
+function sumZero(arr){
+    let left = 0;
+    let right = arr.length -1;
+    while(left < right){
+        let sum = arr[left] + arr[right];
+        if(sum === 0){
+            return [arr[left]],[arr[right]];
+        } else if  (sum > 0) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+}
+
+sumZero([-4,-3,-2,-1,0,1,2,3,10])
