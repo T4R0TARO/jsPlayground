9c8ca9a9441bed909d2d1deffc998fa7bb5fac64
josh manansala <joshmanansalagsd@gmail.com>
2022-01-11 19:22:03 -0800

    algo pract

--- Data Struture and Algo/script.js
+++ Data Struture and Algo/script.js
@@ -248,5 +248,68 @@ const rps = (p1, p2) => {
       return "Player 2 won!";
   };
   
-  
-  
+  //EXAMPLE
+/*
+Write a function called maxSubarraySum which accepts an 
+array of integers and a number called n. The function 
+should calculate the maximum sum of n consecutive 
+elements in the array.
+*/
+maxSubarraySum([1,2,5,2,8,1,5],2) // 10
+maxSubarraySum([1,2,5,2,8,1,5],4) // 17
+maxSubarraySum([4,2,1,6],1) //6
+maxSubarraySum([4,2,1,6,2],4) // 13
+maxSubarraySum([],4) // null 
+
+// SLIDING WINDOW
+/*This pattenr involves creating a WINDOW which can either
+  be an array or number from one position to another.
+
+  Depending on a certain condition, the window either
+  increases or closes (and a new window is created)
+
+  Very useful for keeping track of a subset of data in an
+  array/string etc.
+*/
+
+  // MAX SUM
+  function maxSubarraySum(arr, num) {
+    if ( num > arr.length){
+        return null;
+    }
+    var max = -Infinity;
+    for(let i = 0; i < arr.length - num + 1; i++){
+        temp = 0;
+        for (let j = 0; j < num; j++){
+            temp += arr[i + j];
+        }
+        if (temp > max) {
+            max = temp;
+        }
+        console.log(temp, max)
+    }
+    return max;
+}
+
+  // MAX SUM REFACTOR
+// Time Complexity - O(N)
+function maxSubarraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    //edge case
+    if (arr.length < num) return null;
+    // loop
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    //loop
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum -arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    
+    return maxSum;
+}
+
+maxSubarraySum([2,6,9,2,1,8,5,6,3],3) // 19
