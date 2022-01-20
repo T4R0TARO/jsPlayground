ef49649b25361627dffe8a7c631869f4b3ff8688
josh manansala <joshmanansalagsd@gmail.com>
2022-01-19 17:28:25 -0800

    algo pract

--- Problem Solving Patterns/script1.js
+++ Problem Solving Patterns/script1.js
@@ -41,6 +41,90 @@ console.log(idols.indexOf('gura')); // 2
 
  //REFACTOR 
 
- function sameR(){
+ function sameR(arr1, arr2){
+     //short circut 
+     if(arr1.length !== arr2.length) return false;
      
+     // object counters
+     let frequencyCounter1 = {};
+     let frequencyCounter2 = {};
+
+     //loop over arr1
+     for (let val of arr1){
+         frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+         
+     }
+     //loop over arr2
+     for (let val of arr2){
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+        
+    }
+    //loop over frequencyCounter1
+     for (let key in frequencyCounter1){
+         //if key squared is NOT in frequencyCounter2 return false
+         if(!(key **2 in frequencyCounter2)) return false
+        // if key squared is NOT equal to frequencyCounter[key]
+         if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]) return false
+     }
+     // if all cases are false cases are bypassed return true
+     console.log(frequencyCounter1)
+     console.log(frequencyCounter2)
+     return true
  }
+
+ console.log(sameR([1,2,2,3], [1,4,4,9])) // true
+
+  
+ /** ANAGRAM 
+  * Write a function that accepts two strings
+  * If str1 is NOT the same length as str2 return false 
+  * create loopup object to compare later
+  * loop through str1
+  * loop through str2
+  * Check if str2 is NOT an anagram to str1 return false
+  * return true 
+  */
+
+
+ //Write a function that accepts two strings
+ function anagram(str1, str2){
+     //If str1 is NOT the same length as str2 return false 
+     if (str1.length !== str2.length) return false
+     
+     let lookup = {}
+
+    // loop through str1
+    for (let i = 0; i < str1.length; i++){
+        let letter = str1[i];
+        // if letter exists, increments otherwise set to 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+    // loop through str2
+    for (let i = 0; i < str2.length; i++){
+        let letter = str2[i];
+        //can't find letter or letter is zero then it's not an anagram
+        if(!lookup[letter]) {
+            return false
+        } else {
+            // subtracts the count of letters is objects if all letter become zero then the return true
+            // we do not need to write the logic for zero out because  if 
+            lookup[letter] -= 1; 
+        }
+    }
+    return true;
+ }
+
+{
+    c:1
+    a:1
+    r:1
+}
+{
+    a:3
+    n:1
+    g:1
+    r:1
+    m:1
+}
+console.log(anagram('car', 'arc'))
+console.log(anagram('anagram', 'nagaram'))
