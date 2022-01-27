5b4f61bb26edaa85e6a81568edd2f917464d4a55
josh manansala <joshmanansalagsd@gmail.com>
2022-01-26 20:39:34 -0800

    algo pract

--- Problem Solving Patterns/recursion.js
+++ Problem Solving Patterns/recursion.js
@@ -141,11 +141,27 @@ function outer(input){
     return outerSpaceVariable;
 }
 
-// Recursion_Helper_Method
-function collectOddValues(arr){
-    let result = []
 
+// Recursion_Helper_Method
+/**
+ * Function collectOddValues accept an array
+ * create new results array
+ * function helper accepts a helperInput array
+ * function helpers base case if length is 0 return 
+ * function helper checks if helperInput's first item is odd
+ * if true push helperInput item into result array 
+ * if false then there is no item pushed 
+ * function helper is called again with the result of the rest of the array
+ * helperInput.slice(1) access the array without the 0 index
+ */
+//  outer function is not recursive
+// inner function is recursive
+function collectOddValues(arr){
+    let result = [];
+
+    //
     function helper(helperInput){
+        //base case 
         if(helperInput.length === 0){
             return;
         }
@@ -154,25 +170,81 @@ function collectOddValues(arr){
         }
         helper(helperInput.slice(1))
     }
+
     helper(arr)
     return result;
 }
 
-
+console.log(collectOddValues([1,2,3,4,5,6,7,8,9]))
 //PURE RECURSIONS
+/**
+ * function collectOddValues accepts an array ex. [1,2,3,4,5]
+ * define a NEW array to be empty
+ * edge case; if the array is empty return newArr // [1,2,3,4,5] false
+ * check if the first item in the array is odd // [1,2,3,4,5] true [1]
+ * if true PUSH the first odd number into newArr
+ * have new array equal newArr and concat the results of function collectOddvalues
+ * on the rest of the array
+ * the recusive function collectOddvalues is called [2,3,4,5]
+ * creates a new array
+ * base case checks if the number is not 0 // false  [2]
+ * checks if the first number in the array is odd // false [2]
+ * newArr [] equal and concat the results of function collectOddvalues on the rest of the arrayu
+ * the recusive function collectOddvalues is called // [3,4,5]
+ * creates a new array newArr
+ * checks if the first number of the array is odd // [3,4,5] true
+ * if true PUSH the first item of the array into newArr
+ * have new Array equal newArr.concat the result of the function collectOddValues on the rest of the array
+ * the recursive function collectOddValues is called [4,5]
+ * creates new array
+ * base case checks for 0 // false 
+ * checks if first item in array is odd // false [4,5]
+ * update newArr and concat the results of function collectOddValues
+ * recursive function collecOddValues is called [5]
+ * creates new array
+ * base case checks for 0 // false
+ * condition checks if first item in array is odd // [5] true
+ * push item to new array
+ * update newArr and concat the result of function collectOddvalues
+ * recusive function collectOddvalues is called []
+ * creates new array
+ * base case checks of 0 values // [] true
+ * return newArr
+ */
+
+// arr.slice(1)- give the element on the 1 index
 function collectOddValues(arr){
     let newArr = [];
-    // base point
+    //base case
     if(arr.length === 0) {
         return newArr;
     }
-
+    //condition
     if(arr[0] % 2 !== 0){
         newArr.push(arr[0]);
     }
 
+    //--
     newArr = newArr.concat(collectOddValues(arr.slice(1)))
     return newArr;
 }
 
-console.log(collectOddValues([1,2,3,4,5])) // [1,3,5]
+console.log(collectOddValues([1,2,3,4,5])) // [1,3,5]]
+
+// WE HAVE SEPERATE ARRAYS WE WANT THEM TOGETHER
+//[1].concat(collectOddvalues([2,3,4,5]))
+    //[].concat(collectOddvalues([3,4,5]))
+        //[3].concat(collectOddvalues([4,5]))
+            //[].concat(collectOddvalues([5]))
+                //[5].concat(collectOddvalues([]))
+                 //[]
+
+// concat was waiting for the result to finish
+// 
+//return [1,3,5]
+    
+       
+         
+                 
+                
+                 
