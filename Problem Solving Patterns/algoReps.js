728059af6beac72948e79e9e325594a5c0fda32a
josh manansala <joshmanansalagsd@gmail.com>
2022-02-08 20:53:37 -0800

    algo pract

--- Problem Solving Patterns/algoReps.js
+++ Problem Solving Patterns/algoReps.js
@@ -9,38 +9,46 @@
 /**
  * Write a function that takes two numbers and returns the sum of those numbers
  */
-function add(num1, num2){}
+function add(num1, num2){
+ return num1 + num2;
+}
 
 console.log("add", add(2, 3)); //Expect 5
 
 /**
  * Write a function that takes any number of arguments of type number and returns the sum  of all the arguments 
  */
-function sum() {}
+function sum(...arg) {
+    let results = 0;
+
+    for(let i = 0; i < arg.length; i++){
+        results += arg[i];
+    }
+
+    return results
+}
 
 console.log("sum", sum(2,3,4,5)) // Expect 14
 /**
  * Write a function that takes two string. A and B, and returns whether B is in A in a case-insensitive way
  */
-function stringIncludes() {}
+function stringIncludes(stringA, stringB) {
+    let tempString = stringA.toLowerCase();
+    
+    return tempString.includes(stringB);
+}
 
 console.log(
     "stringIncludes",
     stringIncludes("I drove to New York in a van with my friend", "new")
 ) // Expect true
 
+
 /**
  * Write a function that takes an array of objects, and returns an array of the objects 'name' property, only if that property exists
  */
 function getNames(array) {
-    let results = [];
 
-    for(let i = 0; i < array.length; i++){
-        if(array[i].hasOwnProperty("name")) {
-            results.push(array[i].name)
-        }
-    }
-    return results;
 }
 
     console.log(
