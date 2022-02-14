ce9f1fcb55167acbe694176eab1c0f954bbfc55f
josh manansala <joshmanansalagsd@gmail.com>
2022-02-13 19:07:40 -0800

    algo pract

--- Problem Solving Patterns/arrayCardio.js
+++ Problem Solving Patterns/arrayCardio.js
@@ -45,18 +45,46 @@ console.log(reverseInt(123456789))
 // flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3]
 
 function flatten(oldArr){
+    let newArr = []; 
     
     for(let i = 0; i < oldArr.length; i++){
+ 
+        //Array.isArray() checks if the passed value (oldArr[i]) is an Array 
         if(Array.isArray(oldArr[i])){
-            newArray = newArray.concat(flatten
-                (oldArr[i]))
+            //create newArray and combine the oldArr items together by calling the flatten function recursively
+            newArr = newArr.concat(flatten(oldArr[i]))
         } else {
+            //else push the oldArr item in the newArr
             newArr.push(oldArr[i])
             }
         }
+
+        // return the update newArr
         return newArr
     }
+
 console.log(
     "flatten()",
-    flatten('')
+    flatten([1, 2, 3, [4, 5] ])
 )
+// Write a function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array 
+// capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
+
+
+function capitalizeFirst (arr) {
+    let results = [];
+
+    for(let i = 0; i < arr.length; i++){
+            //First Letter 
+            //arr[i].charAt(0).toUpperCase()
+            //remaining letters
+            //arr[i].slice(1)
+        results.push(arr[i].charAt(0).toUpperCase() + arr[i].slice(1))
+    }
+    
+
+    return results;
+
+}
+
+console.log(capitalizeFirst(['car','taco','banana']))
