518567d7d7b97220ed96ff62db463a07b997d602
josh manansala <joshmanansalagsd@gmail.com>
2022-02-17 20:08:49 -0800

    algo pract

--- Problem Solving Patterns/arrayCardio3.js
+++ Problem Solving Patterns/arrayCardio3.js
@@ -120,29 +120,18 @@ console.log('maxCharacter()',
 //Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz".
 //For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
-function fizzBuzz() {
-    // create loop that outputs numbers 1-100
-    // if number is multiple of 3 print "Fizz"
-    // if number is multiple of 5 print "Buzz"
-    // if number is multiple of both 3 and 5 print "FizzBuzz"
-    // print num or string
-
-    ///////////////////////////////
-    // let output = '';
-    for(let i = 1; i <= 100; i++){
-        
-        if(i % 3 === 0)
-        console.log("Fizz")
-
-        if(i % 5 === 0)
-        console.log("Buzz")
-
-        if(i % 3 === 0 && i % 5 === 0)
-        console.log("FizzBuzz");
-
-        console.log(i)
+function fizzBuzz(){
+    for(let i = 0; i <= 100; i++){
+        if( i % 3 === 0 && i % 5 === 0){
+            console.log('FizzBuzz');
+        } else if(i % 3 === 0) {
+            console.log('Fizz');
+        } else if(i % 5 === 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i);    
+        }
     }
-   
 }
 
 console.log('fizzBuzz()',
--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -1,113 +1,61 @@
 // CHALLENGE 1:  REVERSE A STRING
 // Return a string in reverse 
 // ex. reverseString('hello') === 'olleh';
+function reverseString(){
 
-//split string into an array of char 
-//reverse array 
-//join chars back to string
-const revereseString = str => str.split('').reverse().join('')
-
+}
 console.log('reverseString()',
-    revereseString('hello')
+    reverseString()
 )
-
 // CHALLENGE 2: VALIDATE A PALINDROME
 // Return true if palindrome and false if not 
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
-const isPalindrome = str => {
-    const reversedStr = str.split('').reverse().join('');
-    if (reversedStr === str) return true;
-    return false;
-}
+function isPalindrome(){
 
+}
 console.log('isPalindrome()',
-    isPalindrome('boob')
+    isPalindrome()
 )
+
  // CHALLENGE 3: REVERSE AN INTEGER
  // Return an integer in reverse
  // ex. reverseInt(521) === 125
-const reverseInt = int => {
-    let numToString = int.toString().split('').reverse().join('');
-    let parsedString = parseInt(numToString);
-    return parsedString;
+ function reverseInt(){
+
 }
- 
 console.log('reverseInt()',
-    reverseInt(12345)    
+    reverseInt()
 )
+
  //CHALLENGE 4: CAPITALIZE LETTERS
  //Return a string with the first letter of every word capitalized
  // ex. capitalLettters('i love javascript') === 'I Love Javascript'
- 
- //lowercase
- //split
- //map
- //join
+ function capitalizeLetters(){
 
- function capitalizeLetters(sent) {
-    return sent
-    .toLowerCase()
-    .split(' ')
-    .map(function(word){
-        return word[0].toUpperCase() + word.substr(1);
-    })
-    .join(' ')
- }
- 
-
+}
 console.log('capitalizeLetters()',
-    capitalizeLetters('ina, of the mountain, what is you wisdom.')
+    capitalizeLetters()
 )
+ 
 
 //CHALLENGE 5: MAX CHARACTER
 //Return the character that is most common in a string 
 // ex. maxCharacter('javascript') == 'a'
-
-function maxCharacter(str){
-    let charMap = {};
-    let maxNum = 0;
-    let maxChar = '';
-
-    str.split('').forEach(function(char){
-        if(charMap[char]){
-            charMap[char]++
-        } else {
-            charMap[char] = 1;
-        }
-    });
-
-    // return charMap;
-
-    for(let char in charMap){
-        if(charMap[char] > maxNum){
-            maxNum = charMap[char];
-            maxChar = char;
-        }
-    }
-    return maxChar
+function maxCharacter(){
 
 }
-
 console.log('maxCharacter()',
-    maxCharacter('takodachiii')
+    maxCharacter()
 )
+
 
 //CHALLENGE 6: FIZZBUZZ
 //Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz".
 //For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
 function fizzBuzz(){
-    for(let i = 1; i <= 100; i++){
-        if( i % 3 === 0 && i % 5 === 0)
-        console.log('FizzBuzz')
-        if( i % 3 === 0)
-        console.log('Fizz')
-        if( i % 5 === 0)
-        console.log('Buzz')
-        console.log(i)
-    }
-}
 
-// console.log('fizzBuzz()',
-//     fizzBuzz()
-// )
+}
+console.log('fizzBuzz',
+    fizzBuzz()
+)
