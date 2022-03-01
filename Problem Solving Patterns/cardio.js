e302202707495571043971ea80db893d3383b388
josh manansala <joshmanansalagsd@gmail.com>
2022-02-28 20:07:36 -0800

    algo pract

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -2,8 +2,9 @@
 // Return a string in reverse 
 // ex. reverseString('hello') === 'olleh';
 function reverseString(str){
- return str.split('').reverse().join('')
+  return str.split('').reverse().join('')
 }
+
 
 console.log('reverseString()',
     reverseString('takodachi')
@@ -13,14 +14,9 @@ console.log('reverseString()',
 // Return true if palindrome and false if not 
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 function isPalindrome(str){
-  //reverse str
-  const reverseStr = str.split('').reverse().join('');
-  //compare reverseStr to str
-  if(reverseStr === str){
-    return true;
-  } else {
-    return false;
-  }
+  const reverseString = str.split('').reverse().join('')
+  if(reverseString === str) return true;
+  return false;
 }
 
 console.log('isPalindrome()',
@@ -31,12 +27,7 @@ console.log('isPalindrome()',
  // Return an integer in reverse
  // ex. reverseInt(521) === 125
 function reverseInt(int){
-  //change int to str then reverse string
-  const intToStr = int.toString().split('').reverse().join('');
-  //change str to int
-  const parsed = parseInt(intToStr)
-  //return reversed int
-  return parsed;
+
 }
 
 console.log('reverseInt()',
@@ -47,17 +38,7 @@ console.log('reverseInt()',
  //Return a string with the first letter of every word capitalized
  // ex. capitalLettters('i love javascript') === 'I Love Javascript'
 function capitalLetters(sen){
-//make all char in str lowerCase
-//split str into words
-//map() str to words / uppercase first letter of word / + add substr of word starting at index 1
-//join words back to str
-return sen
-.toLowerCase()
-.split(' ')
-.map(function(word){
-  return word[0].toUpperCase() + word.slice(1)
-})
-.join(' ')
+
 }
 
 console.log('capitalLetters()', 
@@ -68,29 +49,7 @@ console.log('capitalLetters()',
 //Return the character that is most common in a string 
 // ex. maxCharacter('javascript') == 'a'
 function maxCharacter(str){
-// create empty object
-  const charMap = {};
-  let maxNum = 0;
-  let maxChar = '';
-// split string to char
-  str.split('').forEach(function(char){
-    if(charMap[char]){
-      charMap[char]++;
-    } else {
-      charMap[char] = 1;
-    }
-  })
 
-// iterate through charMap obj
-// if charMaps[char] items are greater than empty var have empty var take on the value 
-// return the updated value
-  for(let char in charMap){
-    if(charMap[char] > maxNum){
-      maxNum = charMap[char]
-      maxChar = char
-    }
-  }
-  return maxChar
 }
 
 console.log('maxCharacter()', 
@@ -102,17 +61,7 @@ console.log('maxCharacter()',
 //For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
 function fizzBuzz(){
-  for(let i = 1; i <= 100; i++){
-    if(i % 3 === 0 && i % 5 === 0){
-      console.log('FizzBuzz')
-    } else if (i % 3 === 0){
-      console.log('Fizz')
-    } else if (i % 5 === 0){
-      console.log('Buzz')
-    } else {
-      console.log(i)
-    }
-  }
+
 }
 
 console.log('fizzBuzz()',
@@ -148,7 +97,7 @@ function longestWord(sen) {
   // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
   
   function flattenArray(arrays) {
- 
+
   }
   
   console.log('flattenArray()',
@@ -161,10 +110,9 @@ function longestWord(sen) {
   // ex. 'Dormitory' === 'dirty room##'
   
   function isAnagram(str1, str2) {
-
+ 
   }
 
-  //Helper Function 
 
   
   console.log('isAnagram()',
@@ -176,7 +124,6 @@ function longestWord(sen) {
   // ex. 'hello there' === 'Ifmmp UIfsf'
   
   function letterChanges(str) {
-  
 
   }
   console.log('letterChanges()',
