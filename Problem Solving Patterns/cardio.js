42cd07191940f66102702401e0f91d89b13a742f
josh manansala <joshmanansalagsd@gmail.com>
2022-03-11 21:51:32 -0800

    algo pract

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -2,7 +2,7 @@
 // Return a string in reverse 
 // ex. reverseString('hello') === 'olleh';
 function reverseString(str){  
-
+return str.split('').reverse().join('')
 }
 
 console.log('reverseString()',
