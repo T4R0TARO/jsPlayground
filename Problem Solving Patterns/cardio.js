cb656cf24f224a60db2884b194efd004201e4e90
josh manansala <joshmanansalagsd@gmail.com>
2022-03-15 20:25:43 -0700

    algo pract

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -123,7 +123,14 @@ function longestWord(sen) {
   // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
   
   function chunkArray(arr, len) {
+    const chunkedArr = [];
+    let i = 0;
 
+    while(i < arr.length){
+      chunkedArr.push(arr.slice(i, i + len))
+      i += len
+    }
+    return chunkedArr;
   }
   
   console.log('chunkArray()',
