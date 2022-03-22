a5cc5aa25ebf605e777ddc2149b8d72dd545987d
josh manansala <joshmanansalagsd@gmail.com>
2022-03-21 21:31:45 -0700

    algo pract

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -2,7 +2,7 @@
 // Return a string in reverse 
 // ex. reverseString('hello') === 'olleh';
 function reverseString(str){  
-  return str.split('').reverse().join('')
+
 }
 
 console.log('reverseString()',
@@ -13,9 +13,7 @@ console.log('reverseString()',
 // Return true if palindrome and false if not 
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 function isPalindrome(str){
-  let reverseString = str.split('').reverse().join('');
-  if(str === reverseString) return true;
-  return false;
+
 }
 
 console.log('isPalindrome()',
@@ -26,9 +24,7 @@ console.log('isPalindrome()',
  // Return an integer in reverse
  // ex. reverseInt(521) === 125
 function reverseInt(int){
-  let changeToString = int.toString().split('').reverse().join('');
-  let parsedString = parseInt(changeToString);
-  return parsedString;
+
 }
 
 console.log('reverseInt()',
@@ -39,11 +35,7 @@ console.log('reverseInt()',
  //Return a string with the first letter of every word capitalized
  // ex. capitalLettters('i love javascript') === 'I Love Javascript'
 function capitalLetters(sen){
-  return sen
-  .toLowerCase()
-  .split(' ')
-  .map(word => word[0].toUpperCase() + word.substring(1))
-  .join(' ')
+
 }
 
 console.log('capitalLetters()', 
@@ -54,25 +46,7 @@ console.log('capitalLetters()',
 //Return the character that is most common in a string 
 // ex. maxCharacter('javascript') == 'a'
 function maxCharacter(str){
-  const charMap = {};
-  let maxNum = 0;
-  let maxChar = '';
 
-  str.split('').forEach(function(char){
-    if(charMap[char]){
-      charMap[char]++
-    } else {
-      charMap[char] = 1
-    }
-  })
-
-  for(let char in charMap){
-    if(charMap[char] > maxNum){
-      maxNum = charMap[char]
-      maxChar = char
-    }
-  }
-  return maxChar
 }
 
 console.log('maxCharacter()', 
@@ -84,17 +58,7 @@ console.log('maxCharacter()',
 //For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
 function fizzBuzz(){
-  for(let i = 1; i <= 100; i++){
-    if(i % 3 === 0 && i % 5 === 0){
-      console.log('FizzBuzz')
-    } else if (i % 3 === 0){
-      console.log('Fizz')
-    } else if (i % 5 === 0) {
-      console.log('Buzz')
-    } else {
-      console.log(i)
-    }
-  }
+
 }
 
 console.log('fizzBuzz()',
