12f53fa2c898c8c2b61118f727ead469af10e08a
josh manansala <joshmanansalagsd@gmail.com>
2022-03-23 20:35:30 -0700

    algo pract

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -13,8 +13,7 @@ console.log('reverseString()',
 // Return true if palindrome and false if not 
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 function isPalindrome(str){
-  const reverseString = str.split('').reverse().join('')
-  return str === reverseString
+
 }
 
 console.log('isPalindrome()',
@@ -25,9 +24,7 @@ console.log('isPalindrome()',
  // Return an integer in reverse
  // ex. reverseInt(521) === 125
 function reverseInt(int){
-  let reverseString = int.toString().split('').reverse().join('')
-  let parsedStr = parseInt(reverseString)
-  return parsedStr
+
 }
 
 console.log('reverseInt()',
@@ -38,13 +35,7 @@ console.log('reverseInt()',
  //Return a string with the first letter of every word capitalized
  // ex. capitalLettters('i love javascript') === 'I Love Javascript'
 function capitalLetters(sen){
-  return sen
-  .toLowerCase()
-  .split(' ')
-  .map(function(word){
-    return word[0].toUpperCase() + word.substring(1)
-  })
-  .join(' ')
+
 }
 
 console.log('capitalLetters()', 
@@ -55,27 +46,7 @@ console.log('capitalLetters()',
 //Return the character that is most common in a string 
 // ex. maxCharacter('javascript') == 'a'
 function maxCharacter(str){
-  //init object
-  const charMap = {}
-  let maxNum = 0
-  let maxChar = ''
 
-  // split str by char 
-  str.split('').forEach(function(char){
-    if(charMap[char]){
-      charMap[char]++
-    } else {
-      charMap[char] = 1 
-    }
-  })
-
-  for(let char in charMap){
-    if(charMap[char] > maxNum){
-      maxNum = charMap[char]
-      maxChar = char
-    }
-  }
-  return maxChar
 }
 
 console.log('maxCharacter()', 
@@ -87,17 +58,7 @@ console.log('maxCharacter()',
 //For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
 function fizzBuzz(){
-  for(let i = 1; i <= 100; i++){
-    if(i % 3 === 0 &&  i % 5 === 0){
-      console.log('FizzBuzz')
-    } else if (i % 3 === 0) {
-      console.log('Fizz')
-    } else if (i % 5 === 0) {
-      console.log('Buzz')
-    } else {
-      console.log(i)
-    }
-  }
+
 }
 
 console.log('fizzBuzz()',
