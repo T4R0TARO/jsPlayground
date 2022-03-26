941922c240a7377fe8228440ba5ed791ec6d33f0
josh manansala <joshmanansalagsd@gmail.com>
2022-03-25 19:21:17 -0700

    algo pract

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -14,7 +14,7 @@ console.log('reverseString()',
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 function isPalindrome(str){
   const reverseStr = str.split('').reverse().join('')
-  return str === reverseStr
+  return reverseStr === str
 }
 
 console.log('isPalindrome()',
@@ -25,7 +25,9 @@ console.log('isPalindrome()',
  // Return an integer in reverse
  // ex. reverseInt(521) === 125
 function reverseInt(int){
-
+ const reverseStr = int.toString().split('').reverse().join('')
+ const parsed = parseInt(reverseStr)
+ return parsed
 }
 
 console.log('reverseInt()',
@@ -36,7 +38,13 @@ console.log('reverseInt()',
  //Return a string with the first letter of every word capitalized
  // ex. capitalLettters('i love javascript') === 'I Love Javascript'
 function capitalLetters(sen){
-
+  return sen
+  .toLowerCase()
+  .split(' ')
+  .map(function(word){
+    return word[0].toUpperCase() + word.substring(1)
+  })
+  .join(' ')
 }
 
 console.log('capitalLetters()', 
@@ -47,7 +55,26 @@ console.log('capitalLetters()',
 //Return the character that is most common in a string 
 // ex. maxCharacter('javascript') == 'a'
 function maxCharacter(str){
+  const charMap = {}
+  let maxNum = 0
+  let maxChar = '' 
 
+  str.split('').forEach(function(char){
+    if(charMap[char]){
+      charMap[char]++
+    } else {
+      charMap[char] = 1
+    }
+  })
+
+  for(let char in charMap){
+    if(charMap[char] > maxNum){
+      maxNum = charMap[char]
+      maxChar = char 
+    }
+  }
+
+  return maxChar
 }
 
 console.log('maxCharacter()', 
@@ -59,7 +86,17 @@ console.log('maxCharacter()',
 //For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
 function fizzBuzz(){
-
+  for(let i = 1; i <= 100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+      console.log("FizzBuzz")
+    } else if (i % 3 === 0){
+      console.log("Fizz")
+    } else if (i % 5 === 0){
+      console.log("Buzz")
+    } else {
+      console.log(i)
+    }
+  }
 }
 
 console.log('fizzBuzz()',
