b3ec800d6807a7f3c61088cde0849f6a3665c066
josh manansala <joshmanansalagsd@gmail.com>
2022-04-04 19:52:28 -0700

    algo pract

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -35,7 +35,13 @@ console.log('reverseInt()',
  //Return a string with the first letter of every word capitalized
  // ex. capitalLettters('i love javascript') === 'I Love Javascript'
 function capitalLetters(sen){
-
+  return sen
+  .toLowerCase()
+  .split(' ')
+  .map(function(word){
+    return word[0].toUpperCase() + word.substring(1)
+  })
+  .join(' ')
 }
 
 console.log('capitalLetters()', 
@@ -46,7 +52,27 @@ console.log('capitalLetters()',
 //Return the character that is most common in a string 
 // ex. maxCharacter('javascript') == 'a'
 function maxCharacter(str){
+  const charMap = {}
+  let maxNum = 0
+  let maxChar = ''
 
+
+  str.split('').forEach(function(char){
+    if(charMap[char]){
+      charMap[char]++
+    } else {
+      charMap[char] = 1
+    }
+  })
+
+  for(let char in charMap){
+    if(charMap[char] > maxNum){
+      maxNum = charMap[char]
+      maxChar = char
+    }
+  }
+
+  return maxChar
 }
 
 
@@ -61,7 +87,17 @@ console.log('maxCharacter()',
 //For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
 function fizzBuzz(){
-
+ for(let i = 1; i <= 100; i++){
+   if( i % 3 === 0 && i % 5 === 0){
+     console.log("FizzBuzz")
+   } else if (i % 3 === 0){
+     console.log("Fizz")
+   } else if (i % 5 === 0){
+     console.log("Buzz")
+   } else {
+     console.log(i)
+   }
+ }
 }
 
 // console.log('fizzBuzz()',
