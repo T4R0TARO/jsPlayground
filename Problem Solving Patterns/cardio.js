cd6b6c676c5359d7d09d32825e67e88e20f0584e
josh manansala <joshmanansalagsd@gmail.com>
2022-04-25 21:44:15 -0700

    algo practice

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -89,7 +89,17 @@ console.log('maxCharacter()',
 //For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
 function fizzBuzz(){
-
+  for(let i = 1; i <= 100; i++){
+    if( i % 3 === 0 && i % 5 === 0){
+      console.log('FizzBuzz')
+    } else if ( i % 3 === 0){
+      console.log('Fizz')
+    } else if (i % 5 === 0){
+      console.log('Buzz')
+    } else {
+      console.log(i)
+    }
+  }
 }
 
 // console.log('fizzBuzz()',
