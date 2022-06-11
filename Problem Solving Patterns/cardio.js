9e21c123c078d013ccf395148a432712b7190a27
josh manansala <joshmanansalagsd@gmail.com>
2022-06-11 12:05:48 -0700

    algo pract

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -2,9 +2,7 @@
 // Return a string in reverse
 // ex. reverseString('hello') === 'olleh';
 
-function reverseString(str) {
-  return str.split("").reverse().join("");
-}
+function reverseString(str) {}
 
 console.log("reverseString()", reverseString("takodachi"));
 
@@ -12,36 +10,21 @@ console.log("reverseString()", reverseString("takodachi"));
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 
-function isPalindrome(str) {
-  const reverseString = str.split("").reverse().join("");
-  return reverseString === str;
-}
+function isPalindrome(str) {}
 
 console.log("isPalindrome()", isPalindrome("racecar"));
 
 // CHALLENGE 3: REVERSE AN INTEGER
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
-function reverseInt(int) {
-  let changeToString = int.toString().split("").reverse().join("");
-  let parsedString = parseInt(changeToString);
-  return parsedString;
-}
+function reverseInt(int) {}
 
 console.log("reverseInt()", reverseInt(12345));
 
 //CHALLENGE 4: CAPITALIZE LETTERS
 //Return a string with the first letter of every word capitalized
 // ex. capitalLettters('i love javascript') === 'I Love Javascript'
-function capitalLetters(sen) {
-  return sen
-    .toLowerCase()
-    .split(" ")
-    .map(function (word) {
-      return word[0].toUpperCase() + word.substring(1);
-    })
-    .join(" ");
-}
+function capitalLetters(sen) {}
 
 console.log(
   "capitalLetters()",
@@ -51,29 +34,7 @@ console.log(
 //CHALLENGE 5: MAX CHARACTER
 //Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {
-  const charMap = {};
-  let maxNum = 0;
-  let maxChar = "";
-
-  //split string by char
-  str.split("").forEach(function (char) {
-    if (charMap[char]) {
-      charMap[char]++;
-    } else {
-      charMap[char] = 1;
-    }
-  });
-
-  for (let char in charMap) {
-    // debugger
-    if (charMap[char] > maxNum) {
-      maxNum = charMap[char];
-      maxChar = char;
-    }
-  }
-  return maxChar;
-}
+function maxCharacter(str) {}
 
 console.log("maxCharacter()", maxCharacter("takodachiiii"));
 
@@ -81,19 +42,7 @@ console.log("maxCharacter()", maxCharacter("takodachiiii"));
 //Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz".
 //For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
-function fizzBuzz() {
-  for (let i = 0; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-      console.log("Fizz");
-    } else if (i % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log("i");
-    }
-  }
-}
+function fizzBuzz() {}
 
 // console.log('fizzBuzz()',
 //     fizzBuzz()
@@ -177,7 +126,7 @@ console.log("isAnagram()", isAnagram("elbow", "below!!"));
 // ex. 'hello there' === 'Ifmmp UIfsf'
 
 function letterChanges(str) {
-  //replace char with char specified func
+  //replace char with char specified
   let newStr = str.toLowerCase().replace(/[a-z]/gi, function (char) {
     if (char === "z" || char === "Z") {
       return "a";
