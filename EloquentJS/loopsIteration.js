f325bc4fce85848b3872e7c53d5534de4d0d9f94
josh manansala <joshmanansalagsd@gmail.com>
2022-07-06 19:58:05 -0700

    algo pract

---
+++ EloquentJS/loopsIteration.js
@@ -0,0 +1,54 @@
+// for loop
+
+const counterTestV1 = () => {
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+};
+
+// console.log(counterTestV1());
+
+// do...while statement
+
+const counterTestV2 = () => {
+  let i = 0;
+  do {
+    i += 1;
+    console.log(i);
+  } while (i < 5);
+};
+
+// console.log(counterTestV2());
+
+// while statement
+
+const counterTestV3 = () => {
+  let n = 0;
+  let x = 0;
+  while (n < 3) {
+    n++;
+    x += n;
+    console.log(`n=${n}, x=${x}`);
+  }
+};
+
+console.log(counterTestV3());
+
+// for...in statement
+
+let obj1 = [1, 43, 534, 54, 32, 4355];
+
+const counterTestV4 = (obj) => {
+  let result = [];
+
+  for (let i in obj) {
+    console.log(obj[i]);
+    if (obj[i] % 2 === 0) {
+      result += obj[i];
+    }
+  }
+
+  return result;
+};
+
+console.log(counterTestV4(obj1));
