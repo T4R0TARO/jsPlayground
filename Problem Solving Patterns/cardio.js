8e198ad5398f067e35092dfd343fd4b406afac55
josh manansala <joshmanansalagsd@gmail.com>
2022-07-20 19:32:22 -0700

    algo pract

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -56,28 +56,7 @@ console.log("capitalLetters()", capitalLetters("ina of the mountain, what is you
 // ex. maxCharacter('javascript') == 'a'
 
 // function maxCharacter(str) {}
-const maxCharacter = (str) => {
-  const charMap = {};
-  let maxNum = 0;
-  let maxChar = "";
-
-  str.split("").forEach((char) => {
-    if (charMap[char]) {
-      charMap[char]++;
-    } else {
-      charMap[char] = 1;
-    }
-  });
-
-  for (let char in charMap) {
-    if (charMap[char] > maxNum) {
-      maxNum = charMap[char];
-      maxChar = char;
-    }
-  }
-
-  return maxChar;
-};
+const maxCharacter = (str) => {};
 
 console.log("maxCharacter()", maxCharacter("takodachixxx"));
 
