58861b578429fb93fc9d3da84e3b5b7ca845f441
josh manansala <joshmanansalagsd@gmail.com>
2022-09-03 22:20:41 -0700

    algo pract

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -114,7 +114,18 @@ console.log("longestWord()", longestWord("Ina of the mountain, what is your wisd
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
 
-function chunkArray(arr, len) {}
+function chunkArray(arr, len) {
+  const chunkedArr = [];
+
+  let i = 0;
+
+  while (i < arr.length) {
+    chunkedArr.push(arr.slice(i, i + len));
+
+    i += len;
+  }
+  return chunkedArr;
+}
 
 console.log("chunkArray()", chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
 
