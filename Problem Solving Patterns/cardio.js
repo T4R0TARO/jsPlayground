e0684753948897a68eb77018edf9d86f2b2ae609
josh manansala <joshmanansalagsd@gmail.com>
2022-11-30 17:54:33 -0800

    algo practice

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -186,10 +186,13 @@ console.log("flattenArray()", flattenArray([[1, 2], [3, 4], [5, 6], [7]]));
 // ex. 'elbow' === 'below'
 // ex. 'Dormitory' === 'dirty room##'
 
-function isAnagram(str1, str2) {}
+function isAnagram(str1, str2) {
+  return formatStr(str1) === formatStr(str2);
+}
 
-function formatStr(str) {}
-
+function formatStr(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
 console.log("isAnagram()", isAnagram("elbow", "below!!"));
 
 // CHALLENGE 5: LETTER CHANGES
