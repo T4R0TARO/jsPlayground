f4d78b1537b2f26a9531760292dde6c7eea72005
josh manansala <joshmanansalagsd@gmail.com>
2022-12-07 19:33:27 -0800

    algo practice

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -289,16 +289,16 @@ console.log("seekAndDestroy()", seekAndDestroy([2, 4, 5, 6, "hello"], 2, 6, "hel
 // sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
 
 function sortByHeight(a) {
-  /**
-   * init arr1
-   * init arr2
-   *
-   * iterate through `a`
-   * forEach item 'val' 'i'
-   *
-   * a.forEach((val, i) => (val === -1 ? arr1.push(i) : arr2.push(val)))
-   *
-   */
+  const arr1 = [];
+  const arr2 = [];
+
+  a.forEach((val, i) => (val === -1 ? arr1.push(i) : arr2.push(val)));
+
+  const sortArr = arr2.sort((a, b) => a - b);
+
+  arr1.forEach((val, i) => sortArr.splice(arr1[i], 0, -1));
+
+  return sortArr;
 }
 
 // console.log('sortByHeight()')
