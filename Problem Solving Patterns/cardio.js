02ef31a1e34d3314bf20492ef6b12d86fb4e6988
josh manansala <joshmanansalagsd@gmail.com>
2023-01-03 18:22:09 -0800

    algo practice

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -175,7 +175,21 @@ console.log("isAnagram()", isAnagram("elbow", "below!!"));
 // Z should turn to A
 // ex. 'hello there' === 'Ifmmp UIfsf'
 
-const letterChanges = (str) => {};
+const letterChanges = (str) => {
+  // replace char with char specified by function....
+  let newStr = str.toLowerCase().replace(/[a-z]/gi, (char) => {
+    if (char === "z" || char === "Z") {
+      return "a";
+    } else {
+      // return create string from specified sequence = char code next char
+      return String.fromCharCode(char.charCodeAt() + 1);
+    }
+  });
+
+  // mutate newStr replace vowels with uppercase
+  newStr = newStr.replace(/a|e|i|o|u/gi, (vowel) => vowel.toUpperCase());
+  return newStr;
+};
 
 console.log("letterChanges()", letterChanges("hello there"));
 
