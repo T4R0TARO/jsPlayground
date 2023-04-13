85f4011ed8f5d4721636685bfc46e17c5cf2eccb
josh manansala <joshmanansalagsd@gmail.com>
2023-04-12 20:25:27 -0700

    algo practice

--- Problem Solving Patterns/cardio.js
+++ Problem Solving Patterns/cardio.js
@@ -111,7 +111,6 @@ function maxCharacter(str) {
   //   }
   // }
 
-
   return maxChar;
 }
 
@@ -121,7 +120,23 @@ console.log("maxCharacter()", maxCharacter("takodachixxx"));
 //Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz".
 //For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
-function fizzBuzz() {}
+function fizzBuzz() {
+  // print 1-100
+  // divisible by 3 print 'fizz'
+  // divisible by 5 print 'buzz'
+  // divisible by 3 & 5 'fizzbuzz'
+  for(let i = 0; i < 100; i++){
+    if(i % 3 === 0  && i % 5 === 0){
+      console.log('fizzbuzz')
+    } else if (i % 3 === 0) {
+      console.log('fizz')
+    } else if (i % 5 === 0) {
+      console.log('buzz')
+    } else {
+      console.log(i)
+    }
+  }
+}
 
 console.log("fizzBuzz()", fizzBuzz());
 
